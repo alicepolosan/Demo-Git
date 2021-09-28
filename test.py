409ab7a23609d89b0a6f981db08b38119d968ebb
alicepolosan <alice.polosan@mines-paristech.fr>
2021-09-28 15:55:09 +0200

    dernière version

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 print('ceci est un test !')
 print('3,14')
-print('nouveau changement')print('nouveau changement')
-;kjydkjdy
+print('nouveau changement')
+;kjydkjdy`f;kuyd£frdjts`dydt
+
+print('ligne avant')
+
 print('ligne aprés')
 
 print('à envoyer en ligne')
+
+print('à ne pas envoyer')
